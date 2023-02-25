187ac22038ef2d5cb11f36aea8b54053a9fef661
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-25 17:01:02 -0500

    Exercises: Easy I 10 further explored.

--- exercises/easy1/exercise10.rb
+++ exercises/easy1/exercise10.rb
@@ -38,3 +38,21 @@ class Truck < Vehicle
     6
   end
 end
+
+# Further Exploration
+
+# Would it make sense to define a wheels method in Vehicle even though all of
+# the remaining classes would be overriding it? Why or why not? If you think it
+# does make sense, what method body would you write?
+
+# ---
+# It could make sense to define the wheels method in Vehicle, but I don't think
+# its necessary. This is because we may have other vehicles that have no
+# wheels like a boat or a helicopter. If we had to move it, I would make the
+# the method body like this:
+
+# def wheels
+#   0
+# end
+
+# It all depends on what the classes are using the wheels method for.

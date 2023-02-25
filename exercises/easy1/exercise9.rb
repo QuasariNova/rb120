3b9518fe9478ce55669581a80435a315bc243ced
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-25 16:50:18 -0500

    Exercises: Easy I 9 further explored.

--- exercises/easy1/exercise9.rb
+++ exercises/easy1/exercise9.rb
@@ -26,3 +26,24 @@ puts pudding, butterscotch
 
 # My cat Pudding is 7 years old and has black and white fur.
 # My cat Butterscotch is 10 years old and has tan and white fur.
+
+# An alternative approach to this problem would be to modify the Pet class to
+# accept a colors parameter. If we did this, we wouldn't need to supply an
+# initialize method for Cat.
+
+# Why would we be able to omit the initialize method? Would it be a good idea
+# to modify Pet in this way? Why or why not? How might you deal with some of
+# the problems, if any, that might arise from modifying Pet?
+
+# ---
+# If we intialize all three instance variables inside of Pet, then we do not
+# need to override the initialize method for Cat anymore. Ruby will go down
+# the method lookup path looking for the initialize method when invoked by new,
+# so Pet's initialize would be called. This is because of the concept of
+# inheritance.
+
+# The issue with this is if any other classes inherit from Pet, they would all
+# have to be changed if they override the initialize method and use super to
+# call Pet's initialize method. You could deal with this to a point by making
+# the color parameter have a default parameter, but it may not be necessary for
+# every Pet to have this value.

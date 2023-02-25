c3143e965840e0e806eff106aedb194103c56b8e
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-25 06:54:38 -0500

    Lesson 2: Assignment 2 Exercise 1 answered.

---
+++ lesson2/assignment2/exercise1.rb
@@ -0,0 +1,14 @@
+# Given the below usage of the Person class, code the class definition.
+
+# bob = Person.new('bob')
+# bob.name                  # => 'bob'
+# bob.name = 'Robert'
+# bob.name                  # => 'Robert'
+
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end

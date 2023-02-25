8c02b49d2841653ce4299d4ee46ebe58cc238990
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-25 16:27:17 -0500

    Exercises: Easy I 4 further explored.

--- exercises/easy1/exercise4.rb
+++ exercises/easy1/exercise4.rb
@@ -18,3 +18,24 @@ puts %(book = #{book}.)
 
 # The author of "Snow Crash" is Neil Stephenson.
 # book = "Snow Crash", by Neil Stephenson.
+
+
+# Further Exploration
+
+# What do you think of this way of creating and initializing Book objects? (The
+# two steps are separate.) Would it be better to create and initialize at the
+# same time like in the previous exercise? What potential problems, if any, are
+# introduced by separating the steps?
+
+# ---
+# I prefer to create and initialize at the same time like the previous exercise.
+# Though see both patterns as valid. The main reason why I think that creating
+# and initializing in the case is better is that a book will have a title and
+# an author when its created. If we were to call to_s without having initialized
+# the state, we would get:
+
+# "", by
+
+# So you run the chance of trying to use unitialized instance variables if you
+# when invoking an instance method if you don't manually initialize the instance
+# variables through setters.

d2160fe2cd180bfe740bfc68a7f4fbc02daf1134
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-26 08:30:50 -0500

    Exercises: Easy II 6 further explored.

--- exercises/easy2/exercise6.rb
+++ exercises/easy2/exercise6.rb
@@ -49,3 +49,23 @@ elsif bills_wallet < pennys_wallet
 else
   puts 'Bill and Penny have the same amount of money.'
 end
+
+# Further Exploration
+
+# This example is rather contrived and unrealistic, but this type of situation
+# occurs frequently in applications. Can you think of any applications where
+# protected methods would be desirable?
+
+# ---
+# The main thing I see from it is if you want to keep the way the system works
+# under wraps, but it that secret is what is necessary for it to interact with
+# others.
+
+# Take ranking systems in online games. Typically there is a hidden ELO system
+# Underlying a ranking system. Each players data has an ELO and needs to know
+# other players ELO in order properly increase/decrease after matches. The game
+# however is set up so that players only know that they are "Gold" or
+# "Platinum". The individual player is not intended to see the underlying
+# number. You can't make it public, because then the player would know, but you
+# can't make it private because other player objects wouldn't be able to
+# interact with that value. Being protected makes sense here.
